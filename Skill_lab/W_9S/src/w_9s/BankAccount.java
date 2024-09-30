c750666bd85bda66e6c9a973cbb462a444310ae4
KUSUMBA PHANI SRIKAR <151705602+Phani2603@users.noreply.github.com>
2024-09-30 13:45:11 +0530

    Add files via upload

---
+++ Skill_lab/W_9S/src/w_9s/BankAccount.java
@@ -0,0 +1,26 @@
+package w_9s;
+public class BankAccount {
+    private double balance;
+
+    public BankAccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    public synchronized void deposit(double amount) {
+        balance += amount;
+        System.out.println("Deposited " + amount + ". New balance: " + balance);
+    }
+
+    public synchronized void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            System.out.println("Withdrew " + amount + ". New balance: " + balance);
+        } else {
+            System.out.println("Insufficient funds. Balance: " + balance);
+        }
+    }
+
+    public synchronized double getBalance() {
+        return balance;
+    }
+}
