f056227984824450f8ea2a0f78fff0a1080899d4
KUSUMBA PHANI SRIKAR <151705602+Phani2603@users.noreply.github.com>
2024-10-01 13:10:43 +0530

    Delete Week_8 directory

--- Week_8/src/w8_8a/StringSorter.java
+++
@@ -1,7 +0,0 @@
-package w8_8a;
-
-import java.util.List;
-
-public interface StringSorter {
-    void sort(List<String> list);
-}
--- Week_8/src/w8_8b/EvenNumberDoubler.java
+++
@@ -1,7 +0,0 @@
-package w8_8b;
-
-import java.util.List;
-
-public interface EvenNumberDoubler {
-    List<Integer> doubleEvenNumbers(List<Integer> numbers);
-}
